d3237ef17e182781e1c6d14c5da3fa39d6b06176
buildforgelabsceo-ui <buildforgelabsceo@gmail.com>
2026-04-14 12:44:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-# app.py
 from aitoolhub import create_app
 from flask import send_from_directory
+
+app = create_app('production')  # create app FIRST
 
 @app.route('/sitemap.xml')
 def sitemap():
     return send_from_directory('static', 'sitemap.xml')
-app = create_app('production')  # change to production
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0', port=8080)
